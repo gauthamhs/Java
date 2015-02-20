68879964e043b1fb9fa2d5f45221015edab5431a
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-20 11:39:18 -0500

    Update and rename Multithreading/Multiple-Locks-using-Sychronized-Method to Multithreading/Multiple-Locks-using-Sychronized-Method/Application.java

---
+++ Multithreading/Multiple-Locks-using-Sychronized-Method/Application.java
@@ -0,0 +1,17 @@
+// Using Multiple locks for efficiency in Multithreading using synchronized code blocks.
+
+package third; 
+
+public class Application {
+	public static void main(String[] args) {
+		
+/*		Element1 element1 = new Element1();
+		element1.main();*/
+		
+		
+/*		Element2 element2 = new Element2();
+		element2.main();*/
+/*		Element3 element3 = new Element3();
+		element3.main();*/
+	}
+}
