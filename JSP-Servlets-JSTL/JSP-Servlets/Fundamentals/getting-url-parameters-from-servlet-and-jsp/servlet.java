d7cdf8def4fd0b2f8e7290eb85953c7df0d1c706
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-04-07 17:52:27 -0400

    Create servlet.java
    To get the parameters, type the user and id in the url.
    
    Eg: after the context root, type ?user=something&id=something

---
+++ JSP-Servlets-JSTL/JSP-Servlets/Fundamentals/getting-url-parameters-from-servlet-and-jsp/servlet.java
@@ -0,0 +1,49 @@
+package urlparams;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Application
+ */
+@WebServlet("/Application")
+public class Application extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Application() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		String user = request.getParameter("user");
+		String id = request.getParameter("id");
+		
+		out.println("The user param is: " + user);
+		out.println("The id param is: " + id);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
