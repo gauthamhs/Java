35c2f59cef90f1cf6e57a7bfc872bf0d31946220
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-04-09 00:14:42 -0400

    Create Grades.java

---
+++ JSP-Servlets-JSTL/JSP-Servlets/Useful-Objects/session-without-cookies/Grades.java
@@ -0,0 +1,15 @@
+package user;
+
+public class Grades {
+	private int grades =0;
+
+	public int getGrades() {
+		return grades;
+	}
+
+	public void setGrades(int grades) {
+		this.grades = grades;
+	}
+	
+	
+}
